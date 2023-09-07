de7ab29187fd3d997345a5afa69be6b209a73ad2
Carey99 <edwinakidah1@gmail.com>
2023-09-07 09:28:29 -0700

    Check along with this

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
-int main()
+/**
+ * I am writing my first c progrm
+ */
+int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle\n");
-    return 0;
+	puts("\"Programming is like building a multilingual puzzle\n");
+
+	return (0);
 }
