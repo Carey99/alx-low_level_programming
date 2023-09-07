75eaf4dc060e6488d1d3af6269857c45304dc377
Carey99 <edwinakidah1@gmail.com>
2023-09-07 11:43:01 -0700

    Please Betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
-
-/**
- * I am writing my first c progrm
- */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
