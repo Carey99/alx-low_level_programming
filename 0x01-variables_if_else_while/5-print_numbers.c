b274866f65e6daf34c5ea557b2d58a0adbb81d4c
Carey99 <edwinakidah1@gmail.com>
2023-09-08 16:29:56 -0700

    This prints numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - this program prints digits of base ten
+ * Return:Always 0
+ */
+int main(void)
+{
+	int index;
+
+	for (index = 0; index <= 9; ++index)
+{
+	printf("%d\n", index);
+}
+
+	return (0);
+}
