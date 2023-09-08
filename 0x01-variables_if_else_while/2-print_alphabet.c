2563c18235422da8a8797cae98f375b4f2e4b24a
Carey99 <edwinakidah1@gmail.com>
2023-09-08 15:11:09 -0700

    Both one and two in main function

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,8 +7,9 @@ int main(void)
 {
 	char start = 'a';
 	char end = 'z';
+	char letters;
 
-	for (char letters = start; letters <= end; ++letters)
+	for (letters = start; letters <= end; ++letters)
 {
 	putchar(letters);
 	putchar('\n');
