1585c5a19c065d6d0f33ab490d915637964da8cd
Carey99 <edwinakidah1@gmail.com>
2023-09-10 00:15:21 -0700

    How about printing to a stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
- * main - no much of description for this
- * Return:Alway 1
+ * main - write function is for printing stderr
+ * Return: Always 1
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	const char *message =
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, message, sizeof(message) - 1);
 	return (1);
 }
