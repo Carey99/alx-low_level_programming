88fdf11ecde77b9072cbe0b6ef3061f1603ff175
Carey99 <edwinakidah1@gmail.com>
2023-09-10 00:36:40 -0700

    How about printing to a stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <string.h>
 /**
  * main - write function is for printing stderr
  * Return: Always 1
@@ -8,6 +9,6 @@ int main(void)
 	const char *message =
 		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, message, sizeof(message) - 1);
+	write(2, message, strlen(message));
 	return (1);
 }
