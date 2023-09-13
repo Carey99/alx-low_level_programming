0819dbe5cd5fecef1e8db11a17bd6705bed8bdc3
Carey99 <edwinakidah1@gmail.com>
2023-09-13 02:35:27 -0700

    What is the time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+/**
+* jack_bauer - every minute of the day
+*/
+
+void jack_bauer(void)
+{
+	int hours;
+	int minutes;
+
+	for (hours = 0; hours < 24; hours++)
+	{
+	for (minutes = 0; minutes < 60; minutes++)
+	{
+	}
+	}
+}
