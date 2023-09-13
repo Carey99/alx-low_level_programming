6f961fcd3fddba7167d03b7a835afcd055e86d33
Carey99 <edwinakidah1@gmail.com>
2023-09-13 08:28:34 -0700

    Calibrating with times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,32 +1,38 @@
 #include "main.h"
 #include <stdio.h>
-/**
-* times_table - multiplication table of nine
-*/
 
+/**
+ * times_table - multiplication table of nine
+ */
 void times_table(void)
 {
 	int rows = 9;
-	int column = 9;
+	int columns = 9;
 	int product;
 	int i;
 	int j;
 
 	for (i = 0; i <= rows; i++)
 	{
-	for (j = 0; j <= column; j++)
+	for (j = 0; j <= columns; j++)
 	{
 	product = i * j;
-	if (product < 10)
+	if (j == 0)
 	{
-	_putchar(' ');
-	_putchar(' ');
-	_putchar(product + '0');
+	_putchar('0');
 	}
 	else
 	{
+	_putchar(',');
 	_putchar(' ');
+	if (product < 10)
+	{
+	_putchar(' ');
+	}
+	else
+	{
 	_putchar((product / 10) + '0');
+	}
 	_putchar((product % 10) + '0');
 	}
 	}
