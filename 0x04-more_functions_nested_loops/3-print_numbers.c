b18a10582ac47d030cfc29636d544615ec8319dd
Carey99 <edwinakidah1@gmail.com>
2023-09-14 08:53:48 -0700

    We are printinng numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_numbers - let us print numbers
+*/
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+	_putchar(i);
+	}
+	_putchar('\n');
+}
