845f2d4ccf59232856eeda75b25b6d9cf93bc775
Carey99 <edwinakidah1@gmail.com>
2023-09-14 03:58:12 -0700

    Hahahha this gets me suspicious

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* mul - let us multiply
+* @a: first parameter
+* @b: second parameter
+* Return: product
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
