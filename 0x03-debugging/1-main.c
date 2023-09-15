1f5c135e406442b7875c15d62cffdeb623ee1cb9
Carey99 <edwinakidah1@gmail.com>
2023-09-15 07:25:05 -0700

    Infinite loop avoided

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-* main - iterate to exite the loop
+* main - causes an infinite loop
 * Return: 0
 */
 
@@ -9,7 +9,7 @@ int main(void)
 {
 	int i;
 
-	printf("Infinite loop incoming :(");
+	printf("Infinite loop incoming :(\n");
 
 	i = 0;
 
