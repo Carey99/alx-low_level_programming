67946f899852bab5bb36657d576bbf073eca8826
Carey99 <edwinakidah1@gmail.com>
2023-09-14 17:08:12 -0700

    Huh spacing is quite a thing

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - FizzBuzz is a thing
+* Return: 0 indicating success
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+	if (i % 3 == 0 && i % 5 == 0)
+	{
+	printf("FizzBuzz");
+	}
+	else if (i % 3 == 0)
+	{
+	printf("Fizz");
+	}
+	else if (i % 5 == 0)
+	{
+	printf("Buzz");
+	}
+	else
+	{
+	printf("%d", i);
+	}
+	if (i < 100)
+	{
+	printf(" ");
+	}
+	else
+	{
+	printf("\n");
+	}
+	}
+	return (0);
+}
