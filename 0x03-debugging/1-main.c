5c0204739d572677efcdcb15e554fd95e70ab445
Carey99 <edwinakidah1@gmail.com>
2023-09-15 07:41:43 -0700

    Comment it out

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,15 +9,15 @@ int main(void)
 {
 	int i;
 
-	printf("Infinite loop incoming :(");
+	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-/* The while loop part should be iterated */
-	while (i++ < 10)
+/* The while loop part should be iterated 
+	while (i < 10)
 	{
 	putchar(i);
 	}
-
+*/
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
