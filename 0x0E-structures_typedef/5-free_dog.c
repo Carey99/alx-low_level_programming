6a12c6983bc705944aefb5d182725aa7cb394bd1
Carey99 <edwinakidah1@gmail.com>
2023-10-09 15:54:12 -0700

    Trial and error

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -4,12 +4,10 @@
 /**
 * free_dog - Freee dogs
 * dog_t - Struct type defined
-* @p: Let's point to struct
+* @d: Let's point to struct
 */
 
 void free_dog(dog_t *d)
 {
-	free(d->name);
-	free(d->owner);
 	free(d);
 }
