7f82d924f2863d7dd451822583c5dfccda817e3b
Carey99 <edwinakidah1@gmail.com>
2023-10-11 05:11:25 -0700

    Please work

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+* print_name - Prints name
+* @name: Parmeter to be printed
+* @f: Determines how name is printed
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+
+	putchar(*name);
+}
