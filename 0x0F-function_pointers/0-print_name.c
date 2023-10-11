6942e811e8f09bea777d8391db22e11566d78a15
Carey99 <edwinakidah1@gmail.com>
2023-10-11 09:22:38 -0700

    Get things done

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,18 @@
 #include "function_pointers.h"
 #include <stdio.h>
+
 /**
 * print_name - Prints name
 * @name: Parmeter to be printed
 * @f: Determines how name is printed
+* Return: Nothing
 */
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
+	{
 	return;
-
+	}
 	(*f)(name);
 }
