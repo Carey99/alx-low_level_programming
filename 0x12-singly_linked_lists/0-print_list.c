ecd84ee786369edda86847fdeedec21ea2b8f84d
Carey99 <edwinakidah1@gmail.com>
2023-10-19 11:26:50 -0700

    Rectified the print all task

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,14 +11,18 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t length = 0;
+	int length = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		if (h->str == NULL)
+		{
 		printf("[0] (nil)\n");
+		}
 		else
+		{
 		printf("[%d]%s\n", h->len, h->str);
+		}
 	length++;
 	h = h->next;
 	}
