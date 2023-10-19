4d712de758005235bb076005c562780ab3f02f33
Carey99 <edwinakidah1@gmail.com>
2023-10-19 12:30:47 -0700

    The end addition

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,1 +1,52 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+* add_node_end - adds new node to the end
+* @head: Pointer for the nodes
+* @str: String holding chars
+* Return: Address to the new element
+*/
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	int l = 0;
+	list_t *new;
+	list_t *lastnode;
+
+	if (str == NULL)
+	{
+	return (NULL);
+	}
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+	return (NULL);
+	}
+	while (str[l])
+	l++;
+	lastnode = (*head);
+	new->len = l;
+	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+	free(new);
+	return (NULL);
+	}
+	new->next = NULL;
+	if (*head == NULL)
+	{
+	*head = new;
+	}
+	else
+	{
+	while (lastnode->next != NULL)
+	{
+	lastnode = lastnode->next;
+	}
+	lastnode->next = new;
+	}
+	return (new);
+}
