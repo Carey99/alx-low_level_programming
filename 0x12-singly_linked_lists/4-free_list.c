3e069e979b7768f25bdadc49772b6dfa43e694bd
Carey99 <edwinakidah1@gmail.com>
2023-10-19 12:55:35 -0700

    Freeing list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+* free_list - Free list
+* @head: Freeing from here/first node
+*/
+
+void free_list(list_t *head)
+{
+	if (head)
+	{
+	free_list(head->next);
+	if (head->str)
+	free(head->str);
+	free(head);
+	}
+}
