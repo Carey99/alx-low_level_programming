7a36010b0c9fe3c621d3e78b9110c438bbf63771
Carey99 <edwinakidah1@gmail.com>
2023-10-19 10:29:17 -0700

    Ads to the beginning

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+* add_node - Adds a new node
+* @head: Temp pointer for adding
+* @str: String holding characters
+* Return: Null if failed/ adresss to ne char
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+	return (NULL);
+	}
+	new->str = strdup(str);
+	new->next = (*head);
+	(*head) = new;
+
+	return (new);
+}
