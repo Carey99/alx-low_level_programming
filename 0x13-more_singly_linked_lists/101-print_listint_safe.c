6cd4c6f0bbe4e32004528886c1d8d7dbf6bb8be2
Carey99 <edwinakidah1@gmail.com>
2023-10-24 10:15:08 -0700

    Prints safe version

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* print_listint_safe - Prints list
+* @head: Points to first node
+* Exit: Exit statu is 98
+* Return: nodes if need be
+*/
+
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t nodes = 0;
+	const listint_t *ptr = head;
+
+	if (head == NULL)
+	{
+	exit(98);
+	}
+	while (ptr != NULL)
+	{
+	printf("[%p] %d\n", (void *)ptr, ptr->n);
+	nodes++;
+	if (ptr <= ptr->next)
+	{
+	printf("-> [%p] %d\n", (void *)ptr->next, ptr->next->n);
+	break;
+	}
+	ptr = ptr->next;
+	}
+	return (nodes);
+}
