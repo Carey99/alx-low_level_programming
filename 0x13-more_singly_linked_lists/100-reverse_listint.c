bdf5272936c4faba378334c6bfe394955e19315d
Carey99 <edwinakidah1@gmail.com>
2023-10-24 06:46:52 -0700

    Printing in reverse

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+* reverse_listint - Prints elements
+* @head: Points through nodes listed
+* Return: Pointer to the first node of the reversed list
+*/
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *ptr;
+	listint_t *nodes = 0;
+
+	if (*head == NULL)
+	{
+	return (NULL);
+	}
+	ptr = *head;
+	while (ptr)
+	{
+	if (ptr == NULL)
+	nodes--;
+	ptr = ptr->next;
+	}
+	return (nodes);
+}
