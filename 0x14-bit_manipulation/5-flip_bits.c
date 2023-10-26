ab9503bc2fd90dd3efb70963b444381c396c5feb
Carey99 <edwinakidah1@gmail.com>
2023-10-26 12:19:51 -0700

    Returns number of bits needed to flip numbers

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* flip_bits - Returns the number of bits for flipping
+* @n: Given number
+* @m: Where to get the amount of bits
+* Return: Number of bits
+*/
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int result = n ^ m;
+	unsigned int i = 0;
+
+	while (result)
+	{
+	i += result & 1;
+	result >>= 1;
+	}
+	return (i);
+}
